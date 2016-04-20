10b99addb02c35cd89c59aae6e8c4b4e3cde687d
susu <28025920@qq.com>
2016-04-20 22:04:50 +0800

    创建项目

---
+++ UIGestureRecognizer/UIGestureRecognizer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIGestureRecognizer
+//
+//  Created by  江苏 on 16/4/20.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
